668206e33c179feab87864eac4a294242b043e71
Joe Hand <joe@joeahand.com>
2016-08-29 10:22:04 -0700

    move functions to root; standard

--- bin/cli.js
+++ bin/cli.js
@@ -30,7 +30,8 @@ if (parsedArgs.version) {
 parsedArgs.logspeed = +parsedArgs.logspeed
 if (isNaN(parsedArgs.logspeed)) parsedArgs.logspeed = 200
 
-var command = parsedArgs._.shift() // remove command arg so extensions can parse parsedArgs like normal
+var command = parsedArgs._.shift()
+var extensionArgs = args.splice(3)
 if (!command) require('../usage')('root.txt')
 else run()
 
@@ -39,36 +40,35 @@ function run () {
   if (command === 'share') require('../commands/share')(parsedArgs)
   else if (command === 'download') require('../commands/download')(parsedArgs)
   else {
-    var extensionArgs = args.splice(3) // remove command arg
     tryExecExtension(function (err) {
       if (err) return onerror(err)
     })
+  }
+}
 
-    function tryExecExtension (cb) {
-      var extension = spawn(`dat-${command}`, extensionArgs)
-      debug(`Running executable extension: dat-${command} with args [${extensionArgs}]`)
-      extension.stdout.pipe(process.stdout)
-      extension.stderr.pipe(process.stderr)
-      extension.on('error', function (err) {
-        var isMissingExt = (err.code === 'ENOENT' && err.message.indexOf(`spawn dat-${command} ENOENT`) > -1)
-        if (!isMissingExt) return cb(err)
-        extension.on('close', tryLocalExtension)
-        extension.kill()
-      })
-      extension.on('exit', function (code) {
-        debug('Extension exited with code: ' + code)
-      })
-    }
+function tryExecExtension (cb) {
+  var extension = spawn(`dat-${command}`, extensionArgs)
+  debug(`Running executable extension: dat-${command} with args [${extensionArgs}]`)
+  extension.stdout.pipe(process.stdout)
+  extension.stderr.pipe(process.stderr)
+  extension.on('error', function (err) {
+    var isMissingExt = (err.code === 'ENOENT' && err.message.indexOf(`spawn dat-${command} ENOENT`) > -1)
+    if (!isMissingExt) return cb(err)
+    extension.on('close', tryLocalExtension)
+    extension.kill()
+  })
+  extension.on('exit', function (code) {
+    debug('Extension exited with code: ' + code)
+  })
+}
 
-    function tryLocalExtension () {
-      try {
-        require(`dat-${command}`)(extensionArgs)
-      } catch (err) {
-        var isMissingExt = (err.code === 'MODULE_NOT_FOUND' && err.message.indexOf(`dat-${command}`) > -1)
-        if (!isMissingExt) return onerror(err)
-        return onerror(`dat-${command} extension not found. Make sure you have dat-${command} installed and it is executable.`)
-      }
-    }
+function tryLocalExtension () {
+  try {
+    require(`dat-${command}`)(extensionArgs)
+  } catch (err) {
+    var isMissingExt = (err.code === 'MODULE_NOT_FOUND' && err.message.indexOf(`dat-${command}`) > -1)
+    if (!isMissingExt) return onerror(err)
+    return onerror(`dat-${command} extension not found. Make sure you have dat-${command} installed and it is executable.`)
   }
 }
 
